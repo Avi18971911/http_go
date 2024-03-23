bedabc75c25504b717167cc7b9f30605b0dc3795
avinash6 <avinash6@illinois.edu>
2024-03-23 21:52:10 +0800

    Move the main function

--- main/hello.go
+++ main/hello.go
@@ -1,14 +1,8 @@
 package main
 
-import "fmt"
-
 func Hello(greeting string) string {
 	if greeting == "" {
 		greeting = "World"
 	}
 	return "Hello, " + greeting
-}
-
-func main() {
-	fmt.Println(Hello("Mark"))
 }
